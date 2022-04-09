f49c97e786f279356134be5a2a93832830c1d3fd
Ines Alvergne <i.alvergne@gmail.com>
2022-04-09 12:04:07 -0400

    can update a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 1. As a user, I can list tasks
-  get '/tasks', to: 'tasks#index', as: :index
+  get 'tasks', to: 'tasks#index', as: :index
 
   # 3. As a user, I can add a new task
-  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
 
   # 2. As a user, I can view the details of a task
-  get '/tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
   # 4. As a user, I can edit a task
-  get '/tasks/:id/id', to: 'tasks#edit'
-  path '/tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 
 end
